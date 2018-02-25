f9d02d105399b84f4e1b5ec90267f25f513c7a93
Teodora Szasz <dorha.szasz@gmail.com>
2018-02-25 17:23:40 -0600

    modify game

--- resources/js/game.js
+++ resources/js/game.js
@@ -100,14 +100,14 @@ var render = function () {
 
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
+	ctx.font = "18px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
 	
 	// co-ordinates
 	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
+	ctx.font = "18px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("x: " + parseInt(hero.x) + "y:" + parseInt(hero.y), 232, 32);
